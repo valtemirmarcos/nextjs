e931a8dc627768020f9fffcbcad5dd31b7dbb861
valtemir <valtemir@piramideseaair.com.br>
2021-05-26 09:17:06 -0300

    primeira pagina

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+function Home(){
+    return (<div>
+        <h1>Home2</h1>
+        <Contador />
+    </div>)
+}
+function Contador(){
+    const [contador,setContador] = useState(0);
+    function adicionarContador(){
+        setContador(contador+1);
+    }
+    return (
+        <div>
+            <div>{contador}</div>
+            <button onClick={adicionarContador}>Adicionar</button>
+        </div>
+    );
+}
+export default Home
